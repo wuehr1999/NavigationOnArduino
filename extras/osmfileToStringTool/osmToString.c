e36aa272700aca06daabc4b7368f06df4d305789
jw01979 <jonas.wuehr@stud.th-deg.de>
2020-11-01 13:31:38 +0100

    clang

--- extras/osmfileToStringTool/osmToString.c
+++ extras/osmfileToStringTool/osmToString.c
@@ -4,52 +4,44 @@
 // tool for creating string from .osm, that can be copied in arduino sketch
 void printHelp();
 
-int main(int argc, char *argv[])
-{
-    if(argc == 2)
-    {
-    FILE * fp;
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    FILE *fp;
     FILE *fp2;
 
-    char * line = NULL;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    	fp = fopen(argv[1], "r");
-    	if (fp == NULL){
-		printHelp();
-        	exit(EXIT_FAILURE);}
-
-	char outputFile[80];
-	int length = sprintf(outputFile, "String%s", argv[1]);
-
-   	fp2 = fopen(outputFile, "w");
-    	if (fp2 == NULL){
-		printHelp();
-        	exit(EXIT_FAILURE);}
-
-
-	    while ((read = getline(&line, &len, fp)) != -1) {
-		for(int i = 0; i < read - 1; i++)
-		{
-			fputc (line[i], fp2);
-		}
-	    }
-
-	    fclose(fp);
-	    if (line)
-		free(line);
-	    exit(EXIT_SUCCESS);
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+      printHelp();
+      exit(EXIT_FAILURE);
     }
-    else
-	{
-		printHelp();
-	}
-return 0;
+
+    char outputFile[80];
+    int length = sprintf(outputFile, "String%s", argv[1]);
+
+    fp2 = fopen(outputFile, "w");
+    if (fp2 == NULL) {
+      printHelp();
+      exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+      for (int i = 0; i < read - 1; i++) {
+        fputc(line[i], fp2);
+      }
+    }
+
+    fclose(fp);
+    if (line)
+      free(line);
+    exit(EXIT_SUCCESS);
+  } else {
+    printHelp();
+  }
+  return 0;
 }
 
-void printHelp()
-{
-   printf("Run as ./osmToString <filename>.osm\n");
-}
-
+void printHelp() { printf("Run as ./osmToString <filename>.osm\n"); }
